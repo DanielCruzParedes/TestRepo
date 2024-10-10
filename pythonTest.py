e9af285a7693b526448e1145729ff16231e1fb8b
Daniel Cruz <danielcruzp@unitec.edu>
2024-10-09 22:18:37 -0600

    This should appear in the main branch

--- pythonTest.py
+++ pythonTest.py
@@ -3,5 +3,6 @@ print("This should be in the main branch.")
 print("Now this should be in the new second branch.")
 print("This should appear because I edited it from GitKraken Desktop.")
 
+print("This is a new line of code that I added in the main branch.")
 
 

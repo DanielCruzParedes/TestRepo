5c1bf06abeb398eb743c8f73ad57576373b135e5
Daniel Cruz <danielcruzp@unitec.edu>
2024-10-09 22:04:50 -0600

    Small change

--- pythonTest.py
+++ pythonTest.py
@@ -1,6 +1,7 @@
 print("Hello World")
 print("This should be in the main branch.")
 print("Now this should be in the new second branch.")
+print("This should appear because I edited it from GitKraken Desktop.")
 
 
 
